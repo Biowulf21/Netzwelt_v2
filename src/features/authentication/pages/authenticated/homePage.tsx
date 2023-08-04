595f3af5b211f5a5c14f0b7d40fe990436acac39
James Jilhaney <notail2111@gmail.com>
2023-08-04 11:04:37 +0800

    uses HomePageHook return values to separate business logic and UI

--- src/features/authentication/pages/authenticated/homePage.tsx
+++ src/features/authentication/pages/authenticated/homePage.tsx
@@ -1,84 +1,24 @@
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../contexts/authContextProvider';
+import { useAuth } from '../../contexts/authContextProvider';
+import HomePageHooks from './homePageHooks';
 
-interface Territory {
+export interface Territory {
   id: string;
   name: string;
   parent: null | number; // Assuming parent is a number type, update it accordingly if needed
-};
+}
 
 
-interface HeirarchyTerritory extends Territory {
+export interface HeirarchyTerritory extends Territory {
   children: HeirarchyTerritory[];
 }
 
 
 export default function HomePage() {
   const [territories, setTerritories] = useState<Territory[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
   const { isLoggedIn, setIsLoggedIn } = useAuth();
 
-  const logout = () => {
-    console.log('in logout')
-    setIsLoggedIn(false);
-    console.log('after logout')
-  }
-
-  const fetchTerritories = async () => {
-    try {
-      setIsLoading(true)
-      const result = await axios({
-        method: 'GET',
-        url: 'http://localhost:3000/api/places',
-      });
-
-      if (result.status === 200) {
-        const territoryHeirarchy = orderData(result.data['data'], null);
-        console.log(territoryHeirarchy)
-        setIsLoading(false)
-        setTerritories(territoryHeirarchy);
-      }
-    } catch (error) {
-      if (error.response && error.response.status === 404) {
-        // Handle 404 error (user not found)
-        alert('Error 404: User not found');
-      } else if (error instanceof Error) {
-        // Handle other errors
-        alert('Error: ' + error.message + ' - ' + error.name);
-      } else {
-        // Handle other unknown errors
-        alert('An unknown error occurred.');
-      }
-    }
-  };
-
-  const orderData: HeirarchyTerritory[] = (territories: Territory[], parentId: null | string) => {
-    const children = territories.filter((territory) => territory.parent === parentId);
-
-    return children.map((child) => ({
-      ...child,
-      children: orderData(territories, child.id),
-    }));
-  };
-
-
-  const renderTerritory = (territory: HeirarchyTerritory) => {
-    return (
-      <ul>
-        <li key={territory.id}>
-          {territory.name}
-          {territory.children.length > 0 && (
-            <ul>
-              {territory.children.map((child) => renderTerritory(child))}
-            </ul>
-          )}
-        </li>
-      </ul>
-    );
-  }
-
+  const { fetchTerritories, logout, renderTerritory, orderData, isLoading } = HomePageHooks({ territories, setTerritories });
 
   useEffect(() => {
     fetchTerritories();
