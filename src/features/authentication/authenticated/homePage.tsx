142970bcf054e185789f6b49372f3257eb3ba0d4
James Jilhaney <notail2111@gmail.com>
2023-08-04 09:19:27 +0800

    Small changes

--- src/features/authentication/authenticated/homePage.tsx
+++ src/features/authentication/authenticated/homePage.tsx
@@ -1,28 +1,26 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 type TerritoryType = {
-  id: string,
-  name: string,
-  parent: 1
-}
+  id: string;
+  name: string;
+  parent: number; // Assuming parent is a number type, update it accordingly if needed
+};
 
 export default function HomePage() {
   const [territories, setTerritories] = useState<TerritoryType[]>([]);
 
-
   const fetchTerritories = async () => {
     try {
-
       const result = await axios({
         method: 'GET',
         url: 'http://localhost:3000/api/places',
       });
 
-      if (result.status == 200) return result.data;
-
+      if (result.status === 200) {
+        setTerritories(result.data);
+      }
     } catch (error) {
-
       if (error.response && error.response.status === 404) {
         // Handle 404 error (user not found)
         alert('Error 404: User not found');
@@ -31,18 +29,19 @@ export default function HomePage() {
         alert('Error: ' + error.message + ' - ' + error.name);
       } else {
         // Handle other unknown errors
-        alert("An unknown error occurred.");
-
+        alert('An unknown error occurred.');
       }
-
     }
+  };
 
-    useEffect(() => {
-      fetchTerritories()
+  useEffect(() => {
+    fetchTerritories();
+  }, []); // The dependency array should be []
 
-    }, [])
-
-    return (
+  return (
+    <div>
       <h1>Home Page</h1>
-    )
-  }
+      {/* Render territories data here */}
+    </div>
+  );
+}
