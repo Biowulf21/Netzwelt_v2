55aff80299406d849f4369e70b8712d9ae5578eb
James Jilhaney <notail2111@gmail.com>
2023-08-04 11:03:49 +0800

    Fixed imports

--- src/features/authentication/pages/unauthenticated/loginPage.tsx
+++ src/features/authentication/pages/unauthenticated/loginPage.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import "./login.css";
-import { useAuth } from "../contexts/authContextProvider";
+import { useAuth } from "../../contexts/authContextProvider";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
