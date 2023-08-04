11267b6c6b146eb60fc00e937bfaa54b6479235c
James Jilhaney <notail2111@gmail.com>
2023-08-04 08:30:27 +0800

    created login page

---
+++ src/features/authentication/unauthenticated/loginPage.tsx
@@ -0,0 +1,74 @@
+
+import React, { useState } from "react";
+import "./login.css";
+import { useAuth } from "../contexts/authContextProvider";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function LoginPage() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { isLoggedIn, setIsLoggedIn } = useAuth();
+
+  const navigate = useNavigate();
+
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    console.log('in submit')
+    e.preventDefault();
+
+    // const result = await axios.post('/api/login', { username, password })
+    //
+    // if(result.status == 200){
+    //   setIsLoggedIn(true);
+    // }
+    //
+
+    setIsLoggedIn(true)
+    navigate('/')
+
+
+  }
+
+
+  return (
+    <>
+      <div className="login-container">
+        {isLoggedIn ? <h1>Logged in</h1> : <h1>Not Logged In</h1>}
+        <h2>Login</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button onClick={(e) => handleSubmit(e)} className="btn">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          Don't have an account? <a href="#">Sign Up</a>
+        </p>
+      </div>
+    </>
+  );
+}
